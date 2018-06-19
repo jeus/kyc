7a6f7b3d0f2e7a988b7be3a64dbe13db6f216615
Administrator <alkhandani@gmail.com>
2018-06-19 15:18:15 +0430

    disable image tumbnail not work that.

--- src/main/java/com/b2mark/kyc/Image.java
+++ src/main/java/com/b2mark/kyc/Image.java
@@ -4,6 +4,7 @@ import com.b2mark.kyc.entity.CountryJpaRepository;
 import com.b2mark.kyc.entity.KycJpaRepository;
 import com.b2mark.kyc.enums.ImageType;
 import com.b2mark.kyc.exception.BadRequest;
+import com.b2mark.kyc.exception.ContentNotFound;
 import com.b2mark.kyc.image.storage.StorageService;
 import io.swagger.annotations.Api;
 import org.slf4j.Logger;
@@ -14,6 +15,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/img")
@@ -27,21 +34,67 @@ public class Image {
     private final StorageService storageService;
 
     @Autowired
-    Image( StorageService storageService) {
+    Image(StorageService storageService) {
         this.storageService = storageService;
     }
 
     @GetMapping("/{uid}/{imgtype}")
     @ResponseBody
-    public ResponseEntity<Resource> serveFile(@PathVariable String uid,@PathVariable String imgtype) {
+    public ResponseEntity<Resource> serveFile(@PathVariable String uid, @PathVariable String imgtype) {
 
         ImageType imageType = null;
         if ((imageType = ImageType.fromString(imgtype)) != null) {
-            Resource file = storageService.loadAsResource(imageType,uid);
+            Resource file = storageService.loadAsResource(imageType, uid);
             return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
                     "attachment; filename=\"" + file.getFilename() + "\"").body(file);
         } else {
             throw new BadRequest("imagetype [" + imageType + "] is not valid");
         }
     }
+
+
+
+    //TODO: not work have to change for work
+//    @GetMapping("/tumb/{uid}/{imgtype}")
+//    @ResponseBody
+    public ResponseEntity<BufferedImage> serveTumbFile(@PathVariable String uid, @PathVariable String imgtype) {
+        BufferedImage bufferedImage = null;
+        ImageType imageType = null;
+        if ((imageType = ImageType.fromString(imgtype)) != null) {
+            Resource file = storageService.loadAsResource(imageType, uid);
+            try {
+                File pathToFile = new File(file.getURI());
+                BufferedImage image = ImageIO.read(pathToFile);
+                bufferedImage =
+                        createResizedCopy(image, 400, 400, false);
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            if (bufferedImage != null) {
+                return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + file.getFilename() + "\"").body(bufferedImage);
+            } else {
+                throw new ContentNotFound("Not Found this Image");
+            }
+        } else {
+            throw new BadRequest("imagetype [" + imageType + "] is not valid");
+        }
+    }
+
+    private BufferedImage createResizedCopy(BufferedImage originalImage,
+                                            int scaledWidth, int scaledHeight,
+                                            boolean preserveAlpha) {
+        System.out.println("resizing...");
+        int imageType = preserveAlpha ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+        BufferedImage scaledBI = new BufferedImage(scaledWidth, scaledHeight, imageType);
+        Graphics2D g = scaledBI.createGraphics();
+        if (preserveAlpha) {
+            g.setComposite(AlphaComposite.Src);
+        }
+        g.drawImage(originalImage, 0, 0, scaledWidth, scaledHeight, null);
+        g.dispose();
+        return scaledBI;
+    }
+
 }
